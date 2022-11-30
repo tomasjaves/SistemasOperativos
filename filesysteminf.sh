6f37415f0fdbf72a6a2a76f6db3c1b075b057207
alu0101515458 <alu0101515458@ull.edu.es>
2022-11-30 08:45:40 +0000

    cambios finales

--- filesysteminf.sh
+++ filesysteminf.sh
@@ -29,7 +29,7 @@ s_device=					# variable para la condicion -sdevice
 # FUNCIONES
 error_exit() # si hay algún error con las opciones, se ejecuta esta función.
 {
-	echo "No se pudo ejecutar esa opción, para obtener ayuda ejecute el script junto con -h."
+	echo "No se pudo ejecutar esa opción, para obtener ayuda ejecute el script junto con ${TEXT_YELLOW}-h.${TEXT_RESET}"
         echo -n "$1" 1>&2
         exit 1
 }
@@ -393,22 +393,23 @@ sopen_device() {
 	cuenteo=0																			# creamos una variable "cuenteo" donde almacenaremos el total de los archivos abiertos.
 	almacen=0
 	echo "${TEXT_YELLOW}DISPOSITIVOS REPRESENTADOS EN EL SISTEMA OPERATIVO COMO ARCHIVOS (DEVICE FILES)${TEXT_RESET}"
-	muestra+="${TEXT_PURPLE}Files Filesystem Major Minor Open_files ${TEXT_RESET}\n"
+	muestra+="${TEXT_PURPLE}Files Filesystem Space_ocuppies Major Minor Open_files${TEXT_RESET}\n"
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos
 	for tipo in $system;													# for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	do
 		a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev/) 	 # cogemos los archivos de dispositivos
+		contador=$(df -aht $tipo --total | tr -s ' ' ' ' | grep /dev/ | cut -d' ' -f3) # proporciona la ocupacion de cada tipo de sistema
   	if [[ $a2_ejercicio ]]; then																			 # si se cumple la condición de que existe un archivo de dispositivo, entonces se hace el nºmayor,menor y hacemos el cuenteo.
    		mayor=$(stat -c %t ${a2_ejercicio})
 			mayor=$(echo "obase=10; ibase=16; $mayor;" | bc)
 			menor=$(stat -c %T ${a2_ejercicio})
 			menor=$(echo "obase=10; ibase=16; $menor;" | bc)
 			cuenteo=$(lsof ${a2_ejercicio} | wc -l)										 
-		  muestra+="${tipo} ${a2_ejercicio} ${mayor} ${menor} ${cuenteo-1}\n" # a cuenteo se le resta uno por la cabecera.
+		  muestra+="${tipo} ${a2_ejercicio} $contador ${mayor} ${menor} ${cuenteo-1}\n" # a cuenteo se le resta uno por la cabecera.
 		fi
 		cuenteo=0 # le damos valor 0 para que reitere desde este valo
 	done
-  echo -e $muestra | column -t | sort -k5 -nr
+  echo -e $muestra | column -t | sort -k6 -nr
 }
 
 sopen_user()
@@ -418,7 +419,7 @@ sopen_user()
 	almacen=0																		  # creamos una variable "almacen" donde almacenaremos el total de los archivos abiertos (con cabecera).
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos.
 	echo "${TEXT_YELLOW}TABLA DE ARCHIVOS DE DISPOSITIVO CON USUARIOS ESPECÍFICOS${TEXT_RESET}"
-	muestra+="${TEXT_SALMON}User Files Used Mounted_on Space_occupies Number_total Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_SALMON}User Files Filesystem Space_occupies Open_files Major Minor${TEXT_RESET}\n"
 	for tipo in $system; # for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	do
 		if df -at $tipo | cut -d' ' -f1 | grep /dev/ &> /dev/null; # si existe un archivo de dispositivo del tipo, entonces
@@ -446,7 +447,7 @@ sopen_user()
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k4 -nr
+	echo -e $muestra | column -t | sort -k5 -nr
 }
 
 sopen_device_user()
@@ -455,7 +456,7 @@ sopen_device_user()
 	cuenteo=0 																		# creamos una variable "cuenteo" donde almacenaremos el total de los archivos abiertos -1 (sin cabecera).
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos.
 	echo "${TEXT_YELLOW}TABLA DE ARCHIVOS DE DISPOSITIVO CON USUARIOS ESPECÍFICOS (DEVICE FILES)${TEXT_RESET}"
-	muestra+="${TEXT_SALMON}User Files Used Mounted_on Space_occupies Number_total Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_SALMON}User Files Used Space_occupies Open_files Major Minor${TEXT_RESET}\n"
 	for tipo in $system; # for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	do
 		if df -at $tipo | cut -d' ' -f1 | grep /dev/ &> /dev/null; # si existe un archivo de dispositivo del tipo, entonces
@@ -483,7 +484,7 @@ sopen_device_user()
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k4 -nr
+	echo -e $muestra | column -t | sort -k5 -nr
 }
 
 
@@ -491,7 +492,30 @@ sdevice_table()
 {
 	muestra=  																		# creamos una variable "muestra" donde almacenaremos todas las operaciones.
 	echo "${TEXT_YELLOW}TIPOS DE SISTEMAS DE ARCHIVOS MONTADOS EN EL SISTEMA$TEXT_RESET"
-	muestra+="${TEXT_GREEN}Type Filesystem  Used  Mounted_on Number_of_type Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_GREEN}Type Filesystem Space_occupies Mounted_on Number_of_type Major Minor${TEXT_RESET}\n"
+	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos
+	for tipo in $system;  												# for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
+	  do
+	  a_ejercicio=$(df -aht $tipo | tr -s ' ' ' ' | cut -d' ' -f1,3,6 | sort -k2 -n | tail -n 1) # obtenemos los campos 1(Tipo archivo), 3(Usado) y 6(Montado) de cada tipo de sistema
+	  c_ejercicio=$(mount | cut -d' ' -f5 | sort | grep $tipo | wc -w) 	# proporciona con wc cuantas veces existe cada tipo de sistema
+		a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev) 		# cogemos los archivos de dispositivos
+	  if [[ "$a2_ejercicio" ]]; then																		# si se cumple la condición de que existe un archivo de dispositivo, entonces se hace el nºmayor,menor.
+		mayor=$(stat -c %t ${a2_ejercicio})
+		mayor=$(echo "obase=10; ibase=16; $mayor;" | bc)
+		menor=$(stat -c %T ${a2_ejercicio})
+		menor=$(echo "obase=10; ibase=16; $menor;" | bc)
+	  else																															# si no se cumple la condición, asterisco en nºmayor,menor.
+		mayor=\'*\'
+		menor=\'*\'
+		fi
+	  muestra+="${tipo} ${a_ejercicio} ${c_ejercicio} ${mayor} ${menor}\n"
+	  done
+	echo -e $muestra | column -t | sort -k5 -nr	
+}
+
+sdevice_table_noheader()
+{
+	muestra=  																		# creamos una variable "muestra" donde almacenaremos todas las operaciones.
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos
 	for tipo in $system;  												# for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	  do
@@ -516,7 +540,7 @@ sdevice_table_inv()
 {
 	muestra=  																		# creamos una variable "muestra" donde almacenaremos todas las operaciones.
 	echo "${TEXT_YELLOW}TIPOS DE SISTEMAS DE ARCHIVOS MONTADOS EN EL SISTEMA$TEXT_RESET"
-	muestra+="${TEXT_GREEN}Type Filesystem  Used  Mounted_on Number_of_type Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_GREEN}Type Filesystem Space_occupies Mounted_on Number_of_type Major Minor${TEXT_RESET}\n"
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos
 	for tipo in $system;  												# for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	  do
@@ -566,16 +590,17 @@ sdevice_table_user()
 	cuenteo=0 																		# creamos una variable "cuenteo" donde almacenaremos el total de los archivos abiertos -1 (sin cabecera).
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos.
 	echo "${TEXT_YELLOW}TABLA DE ARCHIVOS DE DISPOSITIVO CON USUARIOS ESPECÍFICOS${TEXT_RESET}"
-	muestra+="${TEXT_SALMON}User Files Used Mounted_on Space_occupies Number_total Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_GREEN}User Files Filesystem Space_occupies Open_files Number_of_type Major Minor${TEXT_RESET}\n"
 	for tipo in $system; # for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	do
 		if df -at $tipo | cut -d' ' -f1 | grep /dev/ &> /dev/null; # si existe un archivo de dispositivo del tipo, entonces
 		then
 			c_ejercicio=$(mount | cut -d' ' -f5 | sort | grep $tipo | wc -w) 							 # proporciona con wc cuantas veces existe cada tipo de sistema
 			a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev)   							 # cogemos los archivos de dispositivos
+			a_ejercicio=$(df -aht $tipo | tr -s ' ' ' ' | cut -d' ' -f1,3,6 | sort -k2 -n | tail -n 1) # obtenemos los campos 1(Tipo archivo), 3(Usado) y 6(Montado) de cada tipo de sistema
 			contador=$(df -aht $tipo --total | tr -s ' ' ' ' | grep /dev/ | cut -d' ' -f3) # proporciona la ocupacion de cada tipo de sistema
 			tipo_archivo=$(df -at $tipo | sort -k2 -n | cut -d' ' -f1 | tail -n 1) 				 # cogemos el archivo de dispositivo
-			for user in $usuario;  						# for de cada usuario dentro de la cadena de usuarios.
+			for user in $usuario;  			# for de cada usuario dentro de la cadena de usuarios.
 			do
 				if id $user &> /dev/null; # si existe la id de usuario, entonces
 				then
@@ -589,13 +614,13 @@ sdevice_table_user()
 						mayor=\'*\'
 						menor=\'*\'
 					fi
-					muestra+="$user ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
+					muestra+="$user $tipo ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
 					cuenteo=0 # le damos valor 0 para que reitere desde este valor.
 				fi
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k6 -nr
+	echo -e $muestra | column -t | sort -k5 -nr
 }
 
 sdevice_table_user_noheader()
@@ -609,9 +634,10 @@ sdevice_table_user_noheader()
 		then
 			c_ejercicio=$(mount | cut -d' ' -f5 | sort | grep $tipo | wc -w) 							 # proporciona con wc cuantas veces existe cada tipo de sistema
 			a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev)   							 # cogemos los archivos de dispositivos
+			a_ejercicio=$(df -aht $tipo | tr -s ' ' ' ' | cut -d' ' -f1,3,6 | sort -k2 -n | tail -n 1) # obtenemos los campos 1(Tipo archivo), 3(Usado) y 6(Montado) de cada tipo de sistema
 			contador=$(df -aht $tipo --total | tr -s ' ' ' ' | grep /dev/ | cut -d' ' -f3) # proporciona la ocupacion de cada tipo de sistema
 			tipo_archivo=$(df -at $tipo | sort -k2 -n | cut -d' ' -f1 | tail -n 1) 				 # cogemos el archivo de dispositivo
-			for user in $usuario;  						# for de cada usuario dentro de la cadena de usuarios.
+			for user in $usuario;  			# for de cada usuario dentro de la cadena de usuarios.
 			do
 				if id $user &> /dev/null; # si existe la id de usuario, entonces
 				then
@@ -625,13 +651,13 @@ sdevice_table_user_noheader()
 						mayor=\'*\'
 						menor=\'*\'
 					fi
-					muestra+="$user ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
+					muestra+="$user $tipo ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
 					cuenteo=0 # le damos valor 0 para que reitere desde este valor.
 				fi
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k6 -nr
+	echo -e $muestra | column -t | sort -k5 -nr
 }
 
 sdevice_table_inv_user()
@@ -640,16 +666,17 @@ sdevice_table_inv_user()
 	cuenteo=0 																		# creamos una variable "cuenteo" donde almacenaremos el total de los archivos abiertos -1 (sin cabecera).
 	system=$(mount | cut -d' ' -f5 | sort | uniq) # cogemos todos los tipos de archivos que hay para poder iterar con ellos.
 	echo "${TEXT_YELLOW}TABLA DE ARCHIVOS DE DISPOSITIVO CON USUARIOS ESPECÍFICOS${TEXT_RESET}"
-	muestra+="${TEXT_SALMON}User Files Used Mounted_on Space_occupies Number_total Major Minor${TEXT_RESET}\n"
+	muestra+="${TEXT_GREEN}User Files Filesystem Space_occupies Open_files Number_of_type Major Minor${TEXT_RESET}\n"
 	for tipo in $system; # for de cada tipo dentro de "system" que almacena todos los tipos de archivos.
 	do
 		if df -at $tipo | cut -d' ' -f1 | grep /dev/ &> /dev/null; # si existe un archivo de dispositivo del tipo, entonces
 		then
 			c_ejercicio=$(mount | cut -d' ' -f5 | sort | grep $tipo | wc -w) 							 # proporciona con wc cuantas veces existe cada tipo de sistema
 			a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev)   							 # cogemos los archivos de dispositivos
+			a_ejercicio=$(df -aht $tipo | tr -s ' ' ' ' | cut -d' ' -f1,3,6 | sort -k2 -n | tail -n 1) # obtenemos los campos 1(Tipo archivo), 3(Usado) y 6(Montado) de cada tipo de sistema
 			contador=$(df -aht $tipo --total | tr -s ' ' ' ' | grep /dev/ | cut -d' ' -f3) # proporciona la ocupacion de cada tipo de sistema
 			tipo_archivo=$(df -at $tipo | sort -k2 -n | cut -d' ' -f1 | tail -n 1) 				 # cogemos el archivo de dispositivo
-			for user in $usuario;  						# for de cada usuario dentro de la cadena de usuarios.
+			for user in $usuario;  			# for de cada usuario dentro de la cadena de usuarios.
 			do
 				if id $user &> /dev/null; # si existe la id de usuario, entonces
 				then
@@ -663,13 +690,13 @@ sdevice_table_inv_user()
 						mayor=\'*\'
 						menor=\'*\'
 					fi
-					muestra+="$user ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
+					muestra+="$user $tipo ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
 					cuenteo=0 # le damos valor 0 para que reitere desde este valor.
 				fi
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k6 -n
+	echo -e $muestra | column -t | sort -k5 -n
 }
 
 sdevice_table_inv_user_noheader()
@@ -683,9 +710,10 @@ sdevice_table_inv_user_noheader()
 		then
 			c_ejercicio=$(mount | cut -d' ' -f5 | sort | grep $tipo | wc -w) 							 # proporciona con wc cuantas veces existe cada tipo de sistema
 			a2_ejercicio=$(mount | grep $tipo | cut -d' ' -f1 | grep /dev)   							 # cogemos los archivos de dispositivos
+			a_ejercicio=$(df -aht $tipo | tr -s ' ' ' ' | cut -d' ' -f1,3,6 | sort -k2 -n | tail -n 1) # obtenemos los campos 1(Tipo archivo), 3(Usado) y 6(Montado) de cada tipo de sistema
 			contador=$(df -aht $tipo --total | tr -s ' ' ' ' | grep /dev/ | cut -d' ' -f3) # proporciona la ocupacion de cada tipo de sistema
 			tipo_archivo=$(df -at $tipo | sort -k2 -n | cut -d' ' -f1 | tail -n 1) 				 # cogemos el archivo de dispositivo
-			for user in $usuario;  						# for de cada usuario dentro de la cadena de usuarios.
+			for user in $usuario;  			# for de cada usuario dentro de la cadena de usuarios.
 			do
 				if id $user &> /dev/null; # si existe la id de usuario, entonces
 				then
@@ -699,13 +727,13 @@ sdevice_table_inv_user_noheader()
 						mayor=\'*\'
 						menor=\'*\'
 					fi
-					muestra+="$user ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
+					muestra+="$user $tipo ${a2_ejercicio} ${contador} ${cuenteo-1} ${c_ejercicio} ${mayor} ${menor}\n"
 					cuenteo=0 # le damos valor 0 para que reitere desde este valor.
 				fi
 			done
 		fi
 	done
-	echo -e $muestra | column -t | sort -k6 -n
+	echo -e $muestra | column -t | sort -k5 -n
 }
 
 # FUNCION PRINCIPAL
@@ -793,6 +821,8 @@ elif [[ "$s_open" ]] && [[ "$usu" ]]; then
 	sopen_user $usuario
 elif [[ "$s_device" ]] && [[ "$invertir" ]]; then
 	sdevice_table_inv
+elif [[ "$s_device" ]] && [[ "$no_header" ]]; then
+	sdevice_table_noheader
 elif [[ "$no_arg" ]]; then
 	table
 elif [[ "$ayuda" ]] ; then
